cfc0b89b0e41a4263fe76d28414fbd4e9052cd07
Tim Kimber <tkimber@mobius.org.uk>
2020-01-17 22:29:12 +0000

    Use host aliases Don't hardcode tests to run Added DOMAIN_CHAIN and DOMAIN_PEM configs

--- test/run-test.sh
+++ test/run-test.sh
@@ -11,33 +11,28 @@ wget --no-clobber https://raw.githubusercontent.com/letsencrypt/pebble/master/te
 # cat /etc/pki/tls/certs/ca-bundle.crt /root/pebble.minica.pem > /root/pebble-ca-bundle.crt
 cat /etc/ssl/certs/ca-certificates.crt /root/pebble.minica.pem > /root/pebble-ca-bundle.crt
 export CURL_CA_BUNDLE=/root/pebble-ca-bundle.crt
+export GETSSL_HOST=getssl.test
 
-curl -X POST -d '{"host":"getssl", "addresses":["10.30.50.4"]}' http://10.30.50.3:8055/add-a
+curl -X POST -d '{"host":"'$GETSSL_HOST'", "addresses":["10.30.50.4"]}' http://10.30.50.3:8055/add-a
+BLUE=$(tput setaf 4)
+RESET=$(tput sgr0)
 
-# Test #1 - http-01 verification
-echo Test \#1 - http-01 verification
+for testpath in /getssl/test/test-config/getssl*.cfg; do
+    testfile=${testpath##*/}
 
-cp /getssl/test/test-config/nginx-ubuntu-no-ssl /etc/nginx/sites-enabled/default
-service nginx restart
-/getssl/getssl -c getssl
-cp /getssl/test/test-config/getssl-http01.cfg /root/.getssl/getssl/getssl.cfg
-/getssl/getssl -f getssl
+    # Test #1 - http-01 verification
+    echo "${BLUE}Test #$((testnum+=1)) - ${testfile} verification ${RESET}"
 
-# Test #2 - http-01 forced renewal
-echo Test \#2 - http-01 forced renewal
-/getssl/getssl getssl -f
+    cp /getssl/test/test-config/nginx-ubuntu-no-ssl /etc/nginx/sites-enabled/default
+    service nginx restart
+    /getssl/getssl -c $GETSSL_HOST
+    cp "$testpath" /root/.getssl/${GETSSL_HOST}/getssl.cfg
+    /getssl/getssl $GETSSL_HOST
 
-# Test cleanup
-rm -r /root/.getssl
+    # Test #2 - http-01 forced renewal
+    echo "${BLUE}Test #$((testnum+=1)) - ${testfile} forced renewal $RESET"
+    /getssl/getssl $GETSSL_HOST -f
 
-# Test #3 - dns-01 verification
-echo Test \#3 - dns-01 verification
-cp /getssl/test/test-config/nginx-ubuntu-no-ssl /etc/nginx/sites-enabled/default
-service nginx restart
-/getssl/getssl -c getssl
-cp /getssl/test/test-config/getssl-dns01.cfg /root/.getssl/getssl/getssl.cfg
-/getssl/getssl getssl
-
-# Test #4 - dns-01 forced renewal
-echo Test \#4 - dns-01 forced renewal
-/getssl/getssl getssl -f
+    # Test cleanup
+    rm -r /root/.getssl
+done
